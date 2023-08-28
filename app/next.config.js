5ecb4247f33c90a4b74991a16c42d5edde983da0
Anton Kachan <aa.kvchvn@gmail.com>
2023-08-28 18:49:13 +0300

    build: add images.remotePatterns into next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.yandex.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
