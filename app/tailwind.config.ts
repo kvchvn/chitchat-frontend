e5eb7a426bd28172d7ce95d94b913cb2ebca649c
Anton Kachan <aa.kvchvn@gmail.com>
2023-09-08 21:51:41 +0300

    feat(tailwind): add utility class "plate-shadow"

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,8 +1,17 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {},
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.plate-shadow': {
+          boxShadow: '0px 2px 4px #BFC4C933, 0px 3px 4px #BFC4C92D, 0px 1px 5px #BFC4C93D',
+        },
+      });
+    }),
+  ],
 };
 export default config;
