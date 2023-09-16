4ade5ddc0064e88cdd8d04115dcbb796be42d1fe
Anton Kachan <aa.kvchvn@gmail.com>
2023-09-16 11:22:05 +0300

    feat(Icon): update props type and some properties of <svg>

--- app/src/ui/icon.tsx
+++ app/src/ui/icon.tsx
@@ -2,14 +2,19 @@ import { IconId } from '@/types';
 
 type IconProps = {
   id: IconId;
-  color: string;
-  size: number;
+  color?: string;
+  size?: number;
   className?: string;
 };
 
 export default function Icon({ id, color, size, className }: IconProps) {
   return (
-    <svg width={size} height={size} fill={color} className={className}>
+    <svg
+      width={size ?? '100%'}
+      height={size ?? '100%'}
+      fill={color ?? '#000000'}
+      className={`${className} mx-auto`}
+    >
       <use xlinkHref={`/svg/global.svg#${id}`} />
     </svg>
   );
