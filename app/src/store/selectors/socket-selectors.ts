c96d03851a0f72599285d1680ddf66b08f44687f
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-15 23:32:55 +0300

    refactor(socket-selectors): correct useSocketActionsSelector

--- app/src/store/selectors/socket-selectors.ts
+++ app/src/store/selectors/socket-selectors.ts
@@ -3,7 +3,4 @@ import { useMergedStore } from '..';
 export const useSocketSelector = () => useMergedStore((state) => state.socket);
 
 export const useSocketActionsSelector = () =>
-  useMergedStore((state) => ({
-    setSocket: state.setSocket,
-    resetSocket: state.resetSocket,
-  }));
+  useMergedStore(({ setSocket, resetSocket }) => ({ setSocket, resetSocket }));
