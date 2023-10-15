f7942ee81d200f61174fef179ccd6016927ef99d
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-15 23:41:48 +0300

    feat(pages/chats): update logic

--- app/src/pages/chats/index.tsx
+++ app/src/pages/chats/index.tsx
@@ -1,47 +1,75 @@
-import { API_ENDPOINTS, ROUTES } from '@/constants';
+import { API_ENDPOINTS, NO_MESSAGES_TEXT, ROUTES } from '@/constants';
 import { customKy } from '@/ky';
 import { logError } from '@/lib';
-import { GetUsersResponse } from '@/types';
+import { useChatActionsSelector, useChatsSelector } from '@/store';
+import { ChatsRecord, Nullable } from '@/types';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { Session, getServerSession } from 'next-auth';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { authOptions } from '../api/auth/[...nextauth]';
 
 export default function HomePage({
-  friends,
+  session,
+  chats: chatsFromProps,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
+  const chats = useChatsSelector();
+  const { setChats } = useChatActionsSelector();
 
-  const handleClick = (friendId: string) => {
-    router.push(ROUTES.chatWith(friendId));
+  const handleClick = (id: string) => {
+    router.push(ROUTES.chat(id));
   };
+
+  useEffect(() => {
+    if (chatsFromProps) {
+      setChats(chatsFromProps);
+    }
+
+    return () => {
+      setChats(null);
+    };
+  }, [chatsFromProps, setChats]);
 
   return (
     <>
       <h2 className="text-2xl font-semibold">Home Page</h2>
-      {!friends && <p>Fetching error</p>}
-      {friends && (
+      {!chats ? (
+        <p>Fetching error</p>
+      ) : (
         <ul className="mt-3">
-          {friends.map((friend) => (
+          {Object.entries(chats).map(([id, { lastMessage, unseenMessagesCount, users }]) => (
             <li
-              key={friend.id}
-              onClick={() => handleClick(friend.id)}
-              className="h-22 flex gap-3 border-y hover:cursor-pointer"
+              key={id}
+              onClick={() => handleClick(id)}
+              className="h-22 flex gap-3 border-y p-2 hover:cursor-pointer"
             >
-              {friend.image && (
-                <Image
-                  src={friend.image}
-                  width={56}
-                  height={56}
-                  alt="User's avatar"
-                  className="shrink-0 rounded-full p-2"
-                />
-              )}
-              <div className="flex h-full flex-col">
-                <p>{friend.name}</p>
-                <p className="text-sm text-gray-500">There are no messages yet.</p>
+              <div>
+                {users.map((user) =>
+                  user.image ? (
+                    <Image
+                      key={user.id}
+                      src={user.image}
+                      width={56}
+                      height={56}
+                      alt="User's avatar"
+                      className="shrink-0 rounded-full p-2"
+                    />
+                  ) : null
+                )}
               </div>
+              <div className="flex h-full flex-col">
+                <p>{users.map((user) => user.name).join(', ')}</p>
+                <p className="text-sm text-gray-500">
+                  {lastMessage ? lastMessage.content : NO_MESSAGES_TEXT}
+                </p>
+              </div>
+              {unseenMessagesCount && lastMessage && lastMessage.senderId !== session.user.id ? (
+                <span className="my-auto ml-auto h-9 w-9 rounded-full bg-blue-400 text-center leading-9 text-white">
+                  {unseenMessagesCount <= 9 ? unseenMessagesCount : '9+'}
+                </span>
+              ) : null}
             </li>
           ))}
         </ul>
@@ -62,17 +90,17 @@ export const getServerSideProps = (async (ctx) => {
     };
   }
 
-  const props: { session: Session } & { friends: GetUsersResponse['friends'] } = {
+  const props: { session: Session } & { chats: Nullable<ChatsRecord> } = {
     session,
-    friends: null,
+    chats: null,
   };
 
   try {
-    const friends: GetUsersResponse['friends'] = await customKy
-      .get(API_ENDPOINTS.user.getFriendsOf(session.user.id))
+    const chats: ChatsRecord = await customKy
+      .get(API_ENDPOINTS.user.getChatsOf(session.user.id))
       .json();
 
-    props.friends = friends;
+    props.chats = chats;
   } catch (err) {
     logError('HomePage (getServerSideProps)', err);
   }
