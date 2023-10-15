74899e43167c4d13fd8f72c4eb8895f593460b3a
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-15 23:35:29 +0300

    feat(store/types): specify type of socket in SocketSlice

--- app/src/store/types.ts
+++ app/src/store/types.ts
@@ -1,11 +1,12 @@
-import { Nullable } from '@/types';
+import { ChatsRecord, CustomSocket, Nullable } from '@/types';
+import { Message } from '@prisma/client';
 import { Socket } from 'socket.io-client';
 import { StateCreator } from 'zustand';
 
 export type ImmerStateCreator<T> = StateCreator<T, [['zustand/immer', never], never], [], T>;
 
 export type SocketSlice = {
-  socket: Nullable<Socket>;
+  socket: Nullable<CustomSocket>;
   setSocket: (socket: Socket) => void;
   resetSocket: () => void;
 };
