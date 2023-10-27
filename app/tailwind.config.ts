ed6f79a65af5a0a22c595fbf6570cc646b00a602
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-27 11:25:29 +0300

    build(tailwind): add /ui directory into "content" field

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -2,7 +2,11 @@ import type { Config } from 'tailwindcss';
 import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {},
   plugins: [
     plugin(function ({ addUtilities }) {
