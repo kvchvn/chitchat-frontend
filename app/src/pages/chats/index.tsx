a719633cfa693ab2d67c6ab24a0dd930ff0e0c60
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-27 11:42:38 +0300

    refactor(chats-page): break markup into smaller components

--- app/src/pages/chats/index.tsx
+++ app/src/pages/chats/index.tsx
@@ -1,26 +1,24 @@
-import { API_ENDPOINTS, NO_MESSAGES_TEXT, ROUTES } from '@/constants';
+import { ChatsList } from '@/components/chats-list';
+import { API_ENDPOINTS, ROUTES } from '@/constants';
 import { customKy } from '@/ky';
 import { logError } from '@/lib';
 import { useChatActionsSelector, useChatsSelector } from '@/store';
 import { ChatsRecord, Nullable } from '@/types';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { Session, getServerSession } from 'next-auth';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { authOptions } from '../api/auth/[...nextauth]';
 
-export default function HomePage({
-  session,
+type ServerSidePropsType = {
+  session: Session;
+  chats: Nullable<ChatsRecord>;
+};
+
+export default function ChatsPage({
   chats: chatsFromProps,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const router = useRouter();
   const chats = useChatsSelector();
   const { setChats } = useChatActionsSelector();
-
-  const handleClick = (id: string) => {
-    router.push(ROUTES.chat(id));
-  };
 
   useEffect(() => {
     if (chatsFromProps) {
@@ -34,46 +32,8 @@ export default function HomePage({
 
   return (
     <>
-      <h2 className="text-2xl font-semibold">Home Page</h2>
-      {!chats ? (
-        <p>Fetching error</p>
-      ) : (
-        <ul className="mt-3">
-          {Object.entries(chats).map(([id, { lastMessage, unseenMessagesCount, users }]) => (
-            <li
-              key={id}
-              onClick={() => handleClick(id)}
-              className="h-22 flex gap-3 border-y p-2 hover:cursor-pointer"
-            >
-              <div>
-                {users.map((user) =>
-                  user.image ? (
-                    <Image
-                      key={user.id}
-                      src={user.image}
-                      width={56}
-                      height={56}
-                      alt="User's avatar"
-                      className="shrink-0 rounded-full p-2"
-                    />
-                  ) : null
-                )}
-              </div>
-              <div className="flex h-full flex-col">
-                <p>{users.map((user) => user.name).join(', ')}</p>
-                <p className="text-sm text-gray-500">
-                  {lastMessage ? lastMessage.content : NO_MESSAGES_TEXT}
-                </p>
-              </div>
-              {unseenMessagesCount && lastMessage && lastMessage.senderId !== session.user.id ? (
-                <span className="my-auto ml-auto h-9 w-9 rounded-full bg-blue-400 text-center leading-9 text-white">
-                  {unseenMessagesCount <= 9 ? unseenMessagesCount : '9+'}
-                </span>
-              ) : null}
-            </li>
-          ))}
-        </ul>
-      )}
+      <h2 className="text-2xl font-semibold">Chats</h2>
+      {chatsFromProps ? chats ? <ChatsList chats={chats} /> : <p>Loading...</p> : <p>Error</p>}
     </>
   );
 }
@@ -90,7 +50,7 @@ export const getServerSideProps = (async (ctx) => {
     };
   }
 
-  const props: { session: Session } & { chats: Nullable<ChatsRecord> } = {
+  const props: ServerSidePropsType = {
     session,
     chats: null,
   };
@@ -106,4 +66,4 @@ export const getServerSideProps = (async (ctx) => {
   }
 
   return { props };
-}) satisfies GetServerSideProps<{ session: Session }>;
+}) satisfies GetServerSideProps;
