47757b96d82792f30b0f13f00a11210f8f05a29e
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-27 19:29:42 +0300

    feat(message-context-menu): implement MessageContextMenu

---
+++ app/src/components/message-context-menu/message-context-menu.tsx
@@ -0,0 +1,88 @@
+import {
+  useMessageContextMenuActionsSelector,
+  useMessageContextMenuSelector,
+  useSocketSelector,
+} from '@/store';
+import { Nullable } from '@/types';
+import { MutableRefObject, useEffect, useLayoutEffect, useRef } from 'react';
+
+type MessageContextMenuProps = {
+  chatId: string;
+  parentRef: MutableRefObject<Nullable<HTMLDivElement>>;
+};
+
+export function MessageContextMenu({ chatId, parentRef }: MessageContextMenuProps) {
+  const { isOpen, chosenMessageId, coordinates } = useMessageContextMenuSelector();
+  const { closeContextMenu } = useMessageContextMenuActionsSelector();
+  const socket = useSocketSelector();
+
+  const contextMenuRef = useRef<Nullable<HTMLDivElement>>(null);
+
+  const handleRemoveMessage = () => {
+    if (socket && chosenMessageId) {
+      socket.emit('message:remove', { chatId, messageId: chosenMessageId });
+    }
+  };
+
+  useLayoutEffect(() => {
+    if (contextMenuRef.current && parentRef.current && coordinates) {
+      const DELTA = 10;
+
+      const { width: contextMenuWidth, height: contextMenuHeight } =
+        contextMenuRef.current.getBoundingClientRect();
+
+      const {
+        width: parentWidth,
+        left: parentLeft,
+        top: parentTop,
+      } = parentRef.current.getBoundingClientRect();
+
+      const left =
+        parentLeft + parentWidth - coordinates.x > contextMenuWidth + DELTA
+          ? coordinates.x
+          : coordinates.x - contextMenuWidth;
+
+      const top =
+        coordinates.y - parentTop < contextMenuHeight + DELTA
+          ? coordinates.y
+          : coordinates.y - contextMenuHeight;
+
+      contextMenuRef.current.style.left = `${left}px`;
+      contextMenuRef.current.style.top = `${top}px`;
+    }
+  });
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+
+      if (
+        !contextMenuRef.current?.contains(target) ||
+        (contextMenuRef.current.contains(target) && target.tagName === 'BUTTON')
+      ) {
+        closeContextMenu();
+      }
+    };
+
+    if (isOpen) {
+      document.body.addEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  });
+
+  return isOpen ? (
+    <div ref={contextMenuRef} className="fixed z-10 border border-black bg-stone-200 py-2">
+      <ul className="flex flex-col gap-1">
+        <li className="cursor-pointer py-1 pl-2 pr-4 text-sm font-light hover:bg-stone-300">
+          <button onClick={handleRemoveMessage}>Remove</button>
+        </li>
+        <li className="cursor-pointer py-1 pl-2 pr-4 text-sm font-light hover:bg-stone-300">
+          <button>Edit</button>
+        </li>
+      </ul>
+    </div>
+  ) : null;
+}
