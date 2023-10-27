427c4bf3b77b2ce55f04159f38d730de239589b2
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-27 11:44:29 +0300

    fix(next.config): correct devIndicators property

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  devIndicators: false,
+  devIndicators: {
+    buildActivity: false,
+  },
   images: {
     remotePatterns: [
       {
