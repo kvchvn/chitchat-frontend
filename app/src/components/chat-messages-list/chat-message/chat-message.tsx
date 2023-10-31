35ec9bc58bbd58f1c3f990afc9157f5068bc403b
Anton Kachan <aa.kvchvn@gmail.com>
2023-10-31 22:31:06 +0300

    feat(chat-message): add icon if message is edited

--- app/src/components/chat-messages-list/chat-message/chat-message.tsx
+++ app/src/components/chat-messages-list/chat-message/chat-message.tsx
@@ -1,5 +1,6 @@
 import { useMessageContextMenuActionsSelector } from '@/store';
 import { ChatRelevant } from '@/types';
+import { Icon } from '@/ui/icon';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
 import styles from './chat-message.module.css';
@@ -27,21 +28,33 @@ export function ChatMessage({ message }: ChatMessageProps) {
   }
 
   return (
-    <li
-      onContextMenu={handleContextMenu}
-      className={classnames(
+    <>
+      <li
+        onContextMenu={handleContextMenu}
+        className={classnames(
           'relative w-fit max-w-[45%] cursor-default whitespace-pre-line border px-3 py-1 overflow-anywhere',
-        {
-          'self-end rounded-t-xl rounded-bl-xl border-black bg-white':
-            message.senderId === session.user.id,
-          'rounded-t-xl rounded-br-xl border-sky-700 bg-sky-400 text-white':
-            message.senderId !== session.user.id,
+          {
+            'self-end rounded-t-xl rounded-bl-xl border-black bg-white':
+              message.senderId === session.user.id,
+            'rounded-t-xl rounded-br-xl border-sky-700 bg-sky-400 text-white':
+              message.senderId !== session.user.id,
             [styles.seen]: message.senderId !== session.user.id && message.isSeen,
             [styles['not-seen']]: message.senderId !== session.user.id && !message.isSeen,
           }
-      )}
-    >
-      {message.content}
-    </li>
+        )}
+      >
+        {message.isEdited && (
+          <span
+            className={classnames('absolute bottom-0 block h-4 w-4 opacity-50', {
+              '-left-5': message.senderId === session.user.id,
+              '-right-5': message.senderId !== session.user.id,
+            })}
+          >
+            <Icon id="pencil" />
+          </span>
+        )}
+        {message.content}
+      </li>
+    </>
   );
 }
