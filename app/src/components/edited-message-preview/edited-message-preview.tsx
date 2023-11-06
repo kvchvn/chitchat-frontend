0db5abb82a18f832986f156f5010f210c803f2f7
Anton Kachan <aa.kvchvn@gmail.com>
2023-11-06 23:08:32 +0300

    styles(edited-message-preview): fix text overflowing

--- app/src/components/edited-message-preview/edited-message-preview.tsx
+++ app/src/components/edited-message-preview/edited-message-preview.tsx
@@ -14,11 +14,11 @@ export function EditedMessagePreview() {
   };
 
   return (
-    <div className="just flex items-center gap-2 bg-stone-300 px-2 py-1">
+    <div className="flex items-center gap-2 bg-stone-300 px-2 py-1">
       <span className="block h-6 w-7 border-r border-black pr-2">
         <Icon id="pencil" />
       </span>
-      <p>{messageContent}</p>
+      <p className="max-w-[80%] truncate">{messageContent}</p>
       <button onClick={handleClose} className="relative ml-auto h-6 w-6 hover:bg-stone-400">
         <Icon id="close-md" />
       </button>
