5ec5fb253c88ff09b3045688e8ec543f21c09892
Anton Kachan <aa.kvchvn@gmail.com>
2023-11-11 12:43:04 +0300

    styles(root-layout): add fixed width for wrapper of main content

--- app/src/layouts/root-layout/root-layout.tsx
+++ app/src/layouts/root-layout/root-layout.tsx
@@ -13,7 +13,7 @@ export function RootLayout({ children, session, socket }: RootLayoutProps) {
     <>
       <main className="flex h-full">
         <Sidebar socket={socket} session={session} />
-        <div className="w-full px-10 py-5">{children}</div>
+        <div className="mx-auto w-[90vw] px-10 py-5">{children}</div>
       </main>
     </>
   );
