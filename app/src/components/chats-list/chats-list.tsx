517f62c4ada1ad7208c2a9ebc06f396429c2410c
Anton Kachan <aa.kvchvn@gmail.com>
2023-11-11 12:41:38 +0300

    fix(chats-list): show message if user has not any chats

--- app/src/components/chats-list/chats-list.tsx
+++ app/src/components/chats-list/chats-list.tsx
@@ -8,9 +8,11 @@ type ChatsListProps = {
 export function ChatsList({ chats }: ChatsListProps) {
   return (
     <ul className="mt-3">
-      {Object.entries(chats).map(([id, chat]) => (
-        <ChatItem key={id} id={id} chat={chat} />
-      ))}
+      {Object.keys(chats).length ? (
+        Object.entries(chats).map(([id, chat]) => <ChatItem key={id} id={id} chat={chat} />)
+      ) : (
+        <p>You have no chats</p>
+      )}
     </ul>
   );
 }
