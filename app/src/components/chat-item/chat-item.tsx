f482b761b6a71e0d23cd0b0a7b55dd8299e8bac2
Anton Kachan <aa.kvchvn@gmail.com>
2023-11-11 12:46:40 +0300

    feat(chat-item): change style of user's avatar if chat is disabled

--- app/src/components/chat-item/chat-item.tsx
+++ app/src/components/chat-item/chat-item.tsx
@@ -1,6 +1,8 @@
 import { DEFAULT_USERNAME, NO_MESSAGES_TEXT, ROUTES } from '@/constants';
 import { ChatsRecord } from '@/types';
 import { UserAvatar } from '@/ui/user-avatar';
+import { getTime } from '@/utils';
+import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
@@ -12,6 +14,7 @@ type ChatItemProps = {
 export function ChatItem({
   id,
   chat: {
+    isDisabled,
     lastMessage,
     unreadMessagesCount,
     users: [friend],
@@ -29,22 +32,36 @@ export function ChatItem({
   }
 
   return (
-    <li onClick={handleClick} className="h-22 flex gap-3 border-t p-2 hover:cursor-pointer">
+    <li
+      onClick={handleClick}
+      className="h-22 flex gap-3 border-t p-2 last-of-type:border-b hover:cursor-pointer hover:bg-stone-100"
+    >
       <div className="relative h-12 w-12 shrink-0">
         <UserAvatar
           username={friend.name}
           src={friend.image}
-          className="rounded-full border-2 border-gray-600"
+          className={classNames('rounded-full border-2 border-gray-600', {
+            'scale-75 grayscale': isDisabled,
+          })}
         />
       </div>
       <div className="flex h-full max-w-[70%] flex-col">
         <p>{friend.name ?? DEFAULT_USERNAME}</p>
-        <p className="truncate text-sm text-gray-500">
-          {lastMessage ? lastMessage.content : NO_MESSAGES_TEXT}
+        <p className="truncate text-sm text-gray-600">
+          {lastMessage ? (
+            <>
+              {lastMessage.content}
+              <span className="ml-1 font-mono text-gray-400">
+                [{getTime(lastMessage.createdAt)}]
+              </span>
+            </>
+          ) : (
+            NO_MESSAGES_TEXT
+          )}
         </p>
       </div>
       {unreadMessagesCount && lastMessage && lastMessage.senderId !== session.user.id ? (
-        <span className="my-auto ml-auto h-9 w-9 rounded-full bg-blue-400 text-center leading-9 text-white">
+        <span className="my-auto ml-auto h-7 w-7 rounded-full bg-orange-500 text-center font-mono leading-7 text-white">
           {unreadMessagesCount <= 9 ? unreadMessagesCount : '9+'}
         </span>
       ) : null}
