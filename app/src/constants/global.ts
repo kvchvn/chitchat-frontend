e5878db864a839d1713def8208521a42cd138b19
Anton Kachan <aa.kvchvn@gmail.com>
2023-11-13 16:30:28 +0300

    refactor(constants): change constant's name to API_ENDPOINTS_ENTITIES

--- app/src/constants/global.ts
+++ app/src/constants/global.ts
@@ -8,27 +8,27 @@ export const ROUTES = {
   chat: (id: string) => `/chats/${id}`,
 } as const;
 
-const API_ENDPOINTS_BASES = {
-  user: 'user',
-  chat: 'chat',
+const API_ENDPOINTS_ENTITIES = {
+  users: 'users',
+  chats: 'chats',
 } as const;
 
 export const API_ENDPOINTS = {
   user: {
-    getById: (userId: string) => `${API_ENDPOINTS_BASES.user}/${userId}`,
-    getAll: (userId: string) => `${API_ENDPOINTS_BASES.user}/${userId}/all`,
-    getChatsOf: (userId: string) => `${API_ENDPOINTS_BASES.user}/${userId}/chats`,
+    getById: (userId: string) => `${API_ENDPOINTS_ENTITIES.users}/${userId}`,
+    getAll: (userId: string) => `${API_ENDPOINTS_ENTITIES.users}/${userId}/all`,
+    getChatsOf: (userId: string) => `${API_ENDPOINTS_ENTITIES.users}/${userId}/chats`,
     sendFriendRequest: ({ from, to }: { from: string; to: string }) =>
-      `${API_ENDPOINTS_BASES.user}/${from}/friend-request?receiverId=${to}`,
+      `${API_ENDPOINTS_ENTITIES.users}/${from}/friend-request?receiverId=${to}`,
     getFriendResponse: ({ from, to }: { from: string; to: string }) =>
-      `${API_ENDPOINTS_BASES.user}/${from}/friend-response?requestSenderId=${to}`,
+      `${API_ENDPOINTS_ENTITIES.users}/${from}/friend-response?requestSenderId=${to}`,
     removeFriend: ({ userId, friendId }: { userId: string; friendId: string }) =>
-      `${API_ENDPOINTS_BASES.user}/${userId}/friend-removal?friendId=${friendId}`,
+      `${API_ENDPOINTS_ENTITIES.users}/${userId}/friend-removal?friendId=${friendId}`,
   },
   chat: {
-    getChat: (id: string) => `${API_ENDPOINTS_BASES.chat}/${id}`,
+    getChat: (id: string) => `${API_ENDPOINTS_ENTITIES.chats}/${id}`,
     sendMessage: ({ chatId, senderId }: { chatId: string; senderId: string }) =>
-      `${API_ENDPOINTS_BASES.chat}/${chatId}?senderId=${senderId}`,
+      `${API_ENDPOINTS_ENTITIES.chats}/${chatId}?senderId=${senderId}`,
   },
 } as const;
 
