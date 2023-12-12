94559795ca36855384c297fa24c6617fcd058565
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-12 22:21:27 +0300

    fix(constants): update USERS_CATEGORIES

--- app/src/constants/users.ts
+++ app/src/constants/users.ts
@@ -1,3 +1,4 @@
+import { UsersCategories, UsersCategoriesName } from '~/types/users';
 import { ROUTES } from './global';
 
 export const USERS_CATEGORIES: Record<
@@ -5,10 +6,16 @@ export const USERS_CATEGORIES: Record<
     (typeof ROUTES.community)[keyof typeof ROUTES.community],
     (typeof ROUTES.community)['base']
   >,
-  string
+  { name: UsersCategoriesName; label: UsersCategories }
 > = {
-  [ROUTES.community.all]: 'All',
-  [ROUTES.community.friends]: 'Friends',
-  [ROUTES.community.incomingRequests]: 'Incoming requests',
-  [ROUTES.community.outcomingRequests]: 'Outcoming requests',
+  [ROUTES.community.all]: { name: UsersCategoriesName.All, label: 'all' },
+  [ROUTES.community.friends]: { name: UsersCategoriesName.Friends, label: 'friends' },
+  [ROUTES.community.incomingRequests]: {
+    name: UsersCategoriesName.IncomingRequests,
+    label: 'incomingRequests',
+  },
+  [ROUTES.community.outcomingRequests]: {
+    name: UsersCategoriesName.OutcomingRequests,
+    label: 'outcomingRequests',
+  },
 };
