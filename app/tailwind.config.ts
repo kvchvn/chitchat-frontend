808a9674b937d5e81b963eb908b612ffea6377dc
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-12 22:14:47 +0300

    dev(tailwind): add /app dir into 'content' array in tailwind.config.ts

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -3,6 +3,7 @@ import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
