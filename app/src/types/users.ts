3d05c4d9deedb51ae4c56d0675565db1718b07b7
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-12 22:23:47 +0300

    feat(types): create and update several types

--- app/src/types/users.ts
+++ app/src/types/users.ts
@@ -2,6 +2,13 @@ import { User } from '@prisma/client';
 
 export type UsersCategories = 'all' | 'friends' | 'incomingRequests' | 'outcomingRequests';
 
+export enum UsersCategoriesName {
+  All = 'All',
+  Friends = 'Friends',
+  IncomingRequests = 'Incoming requests',
+  OutcomingRequests = 'Outcoming requests',
+}
+
 export enum UserStatus {
   Default = 'default',
   Me = 'me',
@@ -13,3 +20,7 @@ export enum UserStatus {
 export type UserRelevant = Omit<User, 'emailVerified'>;
 
 export type UserRelevantWithStatus = UserRelevant & { status: UserStatus };
+
+export type UsersCategoriesCount = {
+  [Property in UsersCategories]: number;
+};
