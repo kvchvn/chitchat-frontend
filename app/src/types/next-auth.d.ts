d676410fda607fda04912e8cd345195d8da541e0
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-12 22:18:51 +0300

    fix(auth): correct extending of Session interface

--- app/src/types/next-auth.d.ts
+++ app/src/types/next-auth.d.ts
@@ -1,9 +1,8 @@
 import 'next-auth';
+import { AdapterUser } from 'next-auth/adapters';
 
 declare module 'next-auth' {
   interface Session {
-    user: {
-      id: string;
-    } & DefaultSession['user'];
+    user: AdapterUser;
   }
 }
