0c759115d06b94e9ba90c626c9694bff494c2c4d
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-12 22:32:23 +0300

    fix(ky): remove ky

--- app/src/ky.ts
+++
@@ -1,8 +0,0 @@
-import ky from 'ky';
-
-export const customKy = ky.create({
-  prefixUrl: process.env.NEXT_PUBLIC_SERVER_URL,
-  hooks: {
-    beforeError: [(error) => error.response.json()],
-  },
-});
