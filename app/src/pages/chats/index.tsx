4e749e65c2a717f615736bdc49c042c37ee4a56f
Anton Kachan <aa.kvchvn@gmail.com>
2023-12-28 11:42:49 +0300

    feat(chats-page): handle server error with ServerErrorFallback

--- app/src/pages/chats/index.tsx
+++ app/src/pages/chats/index.tsx
@@ -1,28 +1,29 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { Session } from 'next-auth';
 import { useEffect } from 'react';
+import { ServerErrorFallback } from '~/components/chats-page/server-error-fallback';
+import { DEFAULT_ERROR_RESPONSE } from '~/constants/global';
 import { useChatActionsSelector, useChatsSelector } from '~/store/selectors/chat-selectors';
 import { ChatsRecord } from '~/types/chats';
-import { Nullable } from '~/types/global';
+import { BasicServerSideProps, Nullable } from '~/types/global';
+import { isErrorResponse } from '~/types/guards';
 import { getUserChats } from '~/utils/api';
 import { getSessionData } from '~/utils/get-session-data';
 import { gsspRedirectToSignIn } from '~/utils/gssp-redirect';
 import { logError } from '~/utils/log-error';
 import { ChatsList } from '../../components/chats-page/chats-list';
 
-type ServerSideProps = {
-  session: Session;
+type ServerSideProps = BasicServerSideProps & {
   chats: Nullable<ChatsRecord>;
 };
 
 export default function ChatsPage({
   chats: chatsFromProps,
+  error,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const chats = useChatsSelector();
   const { setChats, resetChats } = useChatActionsSelector();
 
   console.log('ChatsPage RENDER', chats);
-
   useEffect(() => {
     if (chatsFromProps) {
       setChats(chatsFromProps);
@@ -36,7 +37,9 @@ export default function ChatsPage({
   return (
     <>
       <h2 className="text-2xl font-semibold">Chats</h2>
-      {chatsFromProps ? <ChatsList chats={chats ?? chatsFromProps} /> : <p>Error</p>}
+      <ServerErrorFallback error={error}>
+        {chatsFromProps && <ChatsList chats={chats ?? chatsFromProps} />}
+      </ServerErrorFallback>
     </>
   );
 }
@@ -48,18 +51,22 @@ export const getServerSideProps = (async ({ req, res }) => {
     return gsspRedirectToSignIn();
   }
 
-  const props: ServerSideProps = {
-    session,
-    chats: null,
-  };
+  const props: ServerSideProps = { session, chats: null, error: null };
 
   try {
     const chats = await getUserChats(session.user.id, req.cookies);
 
+    if (!chats) {
+      throw new Error(
+        `Failed to load user's chats (userId=${session.user.id}) in getServerSideProps on ChatsPage.`
+      );
+    }
+
     props.chats = chats;
   } catch (err) {
-    logError('HomePage (getServerSideProps)', err);
+    logError('ChatsPage (getServerSideProps)', err);
+    props.error = isErrorResponse(err) ? err : DEFAULT_ERROR_RESPONSE;
   }
 
   return { props };
-}) satisfies GetServerSideProps;
+}) satisfies GetServerSideProps<ServerSideProps>;
