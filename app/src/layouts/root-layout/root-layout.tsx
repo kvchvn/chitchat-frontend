8ce6e77ecb88de691913de0145e811eec2ccf44a
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-04 16:29:41 +0300

    feat(root-layout): hide navigation bar if there is home-page

--- app/src/layouts/root-layout/root-layout.tsx
+++ app/src/layouts/root-layout/root-layout.tsx
@@ -1,10 +1,14 @@
+import { useRouter } from 'next/router';
 import { PropsWithChildren } from 'react';
+import { ROUTES } from '~/constants/global';
 import { Navigation } from './components/navigation';
 
 export function RootLayout({ children }: PropsWithChildren) {
+  const { pathname } = useRouter();
+
   return (
     <main className="flex h-full">
-      <Navigation />
+      {pathname === ROUTES.home ? null : <Navigation />}
       <div className="mx-auto w-[90vw] px-10 py-5">{children}</div>
     </main>
   );
