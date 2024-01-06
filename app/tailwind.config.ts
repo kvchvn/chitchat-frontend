dc848c3bb80b97a78d13f8ffb0f31fbb9a931aa0
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-06 23:12:19 +0300

    dev(deps): install and init shadcn/ui

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,33 +1,43 @@
 import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['var(--font-onest)'],
         mono: ['var(--font-sometype-mono)'],
       },
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {},
-  plugins: [
-    plugin(function ({ addUtilities }) {
-      addUtilities({
-        '.overflow-anywhere': {
-          overflowWrap: 'anywhere',
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-      });
-    }),
-  ],
-};
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
+
 export default config;
