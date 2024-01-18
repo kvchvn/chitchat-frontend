57933b7c06b4fe47a423d22e5d9d10a57397813e
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-18 23:21:10 +0300

    styles(navigation-item): update styles

--- app/src/layouts/root-layout/components/navigation-item.tsx
+++ app/src/layouts/root-layout/components/navigation-item.tsx
@@ -13,14 +13,17 @@ export function NavigationItem({ href, children }: NavigationItemProps) {
   const isActive = Boolean(href && pathname.startsWith(href));
 
   return (
-    <li className="relative h-14 w-full">
+    <li className="relative h-full w-14">
       {href ? (
         <Link
           href={href}
-          className={classNames('block h-full w-full border-r-[3px] py-3 text-green-600', {
-            'dark:border-primary-bg-darker border-primary-bg-dark': isActive,
-            'hover:border-primary-bg-light border-transparent': !isActive,
-          })}
+          className={classNames(
+            'flex h-full w-full items-center justify-center border-t-[3px] px-2',
+            {
+              'border-primary-bg-dark dark:border-primary-bg-darker': isActive,
+              'border-transparent hover:border-primary-bg-light': !isActive,
+            }
+          )}
         >
           {children}
         </Link>
