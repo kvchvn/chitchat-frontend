a08b862368f43e51a346909be638408755db0618
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-19 23:02:50 +0300

    feat(chat-icon): add warning circle instead of changing icon when new message is received

--- app/src/layouts/root-layout/components/chat-icon.tsx
+++ app/src/layouts/root-layout/components/chat-icon.tsx
@@ -4,5 +4,12 @@ import { useUnreadChatsIdsCount } from '~/store/selectors/chat-selectors';
 export function ChatIcon() {
   const unreadChatsIdsCount = useUnreadChatsIdsCount();
 
-  return unreadChatsIdsCount ? <Icon id="chat-unread" /> : <Icon id="chat" />;
+  return (
+    <div className="relative h-full">
+      <Icon id="chat" />
+      {unreadChatsIdsCount ? (
+        <span className="absolute right-1 top-2 h-2 w-2 rounded-full border border-primary-outline-dark bg-warning-base-light dark:border-primary-outline-light dark:bg-warning-base-dark" />
+      ) : null}
+    </div>
+  );
 }
