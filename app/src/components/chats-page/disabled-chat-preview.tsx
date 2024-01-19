a57abccb4a6a05325f8d73c250b9181ee13c44ac
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-19 23:03:51 +0300

    feat(disabled-chat-preview): create DisabledChatPreview component

---
+++ app/src/components/chats-page/disabled-chat-preview.tsx
@@ -0,0 +1,9 @@
+export function DisabledChatPreview() {
+  return (
+    <div className="max-w-[60%]">
+      <p className="text-sm font-light text-error-base-light dark:text-error-base-dark">
+        (Disabled chat)
+      </p>
+    </div>
+  );
+}
