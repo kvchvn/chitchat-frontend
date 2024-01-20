ec286dff4829e64b207796689c094f10e9471810
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-20 20:23:02 +0300

    style(disabled-chat-preview): update text's style

--- app/src/components/chats-page/disabled-chat-preview.tsx
+++ app/src/components/chats-page/disabled-chat-preview.tsx
@@ -1,9 +1,7 @@
 export function DisabledChatPreview() {
   return (
     <div className="max-w-[60%]">
-      <p className="text-sm font-light text-error-base-light dark:text-error-base-dark">
-        (Disabled chat)
-      </p>
+      <p className="text-sm font-light italic text-gray-400">Disabled chat</p>
     </div>
   );
 }
