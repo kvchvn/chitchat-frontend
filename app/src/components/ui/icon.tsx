815e39d09328948a04961f86ab93815e47aefea2
Anton Kachan <aa.kvchvn@gmail.com>
2024-01-29 13:02:38 +0300

    fix(icon): remove size, fill from props and update className

--- app/src/components/ui/icon.tsx
+++ app/src/components/ui/icon.tsx
@@ -2,18 +2,13 @@ import { IconId } from '~/types/global';
 
 type IconProps = {
   id: IconId;
-  color?: string;
-  size?: number;
   className?: string;
 };
 
-export function Icon({ id, color, size, className = '' }: IconProps) {
+export function Icon({ id, className = '' }: IconProps) {
   return (
     <svg
-      width={size ?? '100%'}
-      height={size ?? '100%'}
-      fill={color ?? '#000000'}
-      className={`${className} fill-primary-outline-dark dark:fill-primary-outline-light mx-auto`}
+      className={`h-full w-full fill-primary-outline-dark dark:fill-primary-outline-light ${className}`}
     >
       <use xlinkHref={`/svg/global.svg#${id}`} />
     </svg>
